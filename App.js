aa22654513a7408f90caf7e36f7b20fda287eb73
victorbruce <victorbruce82@gmail.com>
2020-01-24 23:28:08 +0000

    Add state and props to component

--- App.js
+++ App.js
@@ -1,19 +1,37 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
-    </View>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      age: 5,
+      name: "React Native"
+    };
+  }
+
+  increaseAge = () => {
+    this.setState({ age: this.state.age + 1 });
+  };
+  render() {
+    const { name, age } = this.state;
+    return (
+      <View style={styles.container}>
+        <Text>Hello {name}!</Text>
+        <Text>Age: {age}</Text>
+        <Button onPress={this.increaseAge} title="Increase Age" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
+
+export default App;
