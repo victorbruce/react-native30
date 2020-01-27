7cff575cd015a7683b90f9b2ac3c4ad7fbaf05d1
victorbruce <victorbruce82@gmail.com>
2020-01-27 21:32:57 +0000

    Handling input text

--- App.js
+++ App.js
@@ -1,29 +1,40 @@
-import React, { Component } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      age: 5,
-      name: "React Native"
-    };
-  }
+const App = () => {
+  const [name, setName] = useState("React Native");
+  const [age, setAge] = useState(20);
 
-  increaseAge = () => {
-    this.setState({ age: this.state.age + 1 });
+  const handleNameChange = val => {
+    setName(val);
   };
-  render() {
-    const { name, age } = this.state;
-    return (
-      <View style={styles.container}>
-        <Text>Hello {name}!</Text>
-        <Text>Age: {age}</Text>
-        <Button onPress={this.increaseAge} title="Increase Age" />
+
+  const handleAgeChange = val => {
+    setAge(val);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>Name: {name}</Text>
+      <Text>Age: {age}</Text>
+      <View>
+        <Text>Enter name:</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="eg. John Doe"
+          onChangeText={handleNameChange}
+        />
+        <Text>Enter age:</Text>
+        <TextInput
+          keyboardType="numeric"
+          style={styles.input}
+          placeholder="eg. 12"
+          onChangeText={handleAgeChange}
+        />
       </View>
-    );
-  }
-}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -31,6 +42,13 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center"
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#777",
+    margin: 10,
+    padding: 8,
+    width: 200
   }
 });
 
